2604dd95d40634a7148054f415a1a6ab0f9cf14d
krishna <krishna@gmail.com>
2022-11-22 14:27:02 +0530

    Add project files.

---
+++ GenericsPoc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GenericsPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generics Means General form of, means does't specify any data type.
+
+            DataStore<String> store = new DataStore<string>();
+            store.Data = "Hello World";
+            Console.WriteLine(store.Data);
+        }
+    }
+
+    class DataStore<T>
+    {
+        public T Data { get; set; }
+    }
+
+    class DataStoreArray<T>
+    {
+        public T[] data = new T[10];
+    }
+
+    class KeyValuePair<TKey,TValue>
+    {
+        public TValue Value { get; set; }
+        public TKey Key { get; set; }
+    }
+}
